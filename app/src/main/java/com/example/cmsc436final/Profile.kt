49f93a5afb82c58a813dfc2c4450b29b0a5f9336
Roderick <roderickli1189@gmail.com>
2024-11-16 23:40:51 -0500

    initial commit

--- app/src/main/java/com/example/cmsc436final/Profile.kt
+++ app/src/main/java/com/example/cmsc436final/Profile.kt
@@ -10,7 +10,7 @@ import android.view.ViewGroup
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+ 
 /**
  * A simple [Fragment] subclass.
  * Use the [Profile.newInstance] factory method to
